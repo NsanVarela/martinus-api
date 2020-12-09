85a7fe9fbc6100c71b0707f2d7461f252ebcbfd3
NsanV <nsanv.dev@gmail.com>
2020-12-09 21:14:03 +0100

    service newsletter

--- api/server.js
+++ api/server.js
@@ -25,20 +25,57 @@ router.route('/')
   })
 });
 
-router.route('/news')
+router.route('/newsletter')
 .post(function(req,res) {
-  res.json({ 
-    message : `Demande une inscription à la newsletter de la Team Martinus`,
-    name : req.body.name,
-    firstname : req.body.firstname,
-    email: req.body.email,
-    methode : req.method
-  })
+  console.log(`From Website on newsletter : `, req.body)
+
+  const output = `
+    <p>Vous avez une nouvelle d'inscription à la newsletter'</p>
+    <h3>Email du contact</h3>
+    <ul>
+      <li>Email : ${req.body.contact.email}</li>
+    </ul>
+  `;
+  const mailOptions = {
+    from: "Contact depuis le site :" + req.body.contact.email,
+    to: `${dev}, ${contact}`,
+    subject: `Votre newsletter`,
+    text: `Inscripton à la newsletter`,
+    html: output,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({
+        status: 'fail'
+      })
+    }  else {
+      // console.log(`Message sent: %s ${info.messageId}`);
+      // Preview only available when sending through an Ethereal account
+      // console.log(`Preview URL: %s ${nodemailer.getTestMessageUrl(info)}`);
+      res.json({
+        status: 'success',
+        message: `Merci, votre email nous servira à vous adresser la prochaine newsletter !`
+      })
+    }
+    transporter.sendMail({
+      from: contact,
+      to: req.body.contact.email,
+      subject: `Email envoyé avec succès.`,
+      text: ` Merci de nous avoir contactés ! 
+        Le prochain numéro de la newsletter vous sera adressé par email`
+    }, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Message sent: ' + info.response);
+      }
+    })
+  });
 });
 
 router.route('/contact')
 .post(function(req,res) {
-  // console.log(`From Website : `, req.body)
+  console.log(`From Website on contact : `, req.body)
 
   const output = `
     <p>Vous avez une nouvelle demande de contact</p>
