7ad6b6666d7fee117782c81b18bac18588c3ea0c
NsanV <nsanv.dev@gmail.com>
2020-12-11 12:29:34 +0100

    handle shop request

--- api/server.js
+++ api/server.js
@@ -1,21 +1,20 @@
-const express = require("express")
-const app = express()
-const port = 3000
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const nodemailer = require('nodemailer')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-const router = express.Router()
-const config = require("../environments/environment")
-const transporter = require('./services/mail.service')
+const router = express.Router();
+const config = require("../environments/environment");
+const transporter = require('./services/mail.service');
 
-const dev = config.hosting.DEV
-const contact = config.hosting.CONTACT
-const sebastien = config.hosting.SEBASTIEN
-const deivid = config.hosting.DEIVID
+const dev = config.hosting.DEV;
+const contact = config.hosting.CONTACT;
+const sebastien = config.hosting.SEBASTIEN;
+const deivid = config.hosting.DEIVID;
 
 router.route('/')
 .all(function(req,res) { 
@@ -27,8 +26,7 @@ router.route('/')
 
 router.route('/newsletter')
 .post(function(req,res) {
-  console.log(`From Website on newsletter : `, req.body)
-
+  // console.log(`From Website on newsletter : `, req.body)
   const output = `
     <p>Vous avez une nouvelle d'inscription à la newsletter'</p>
     <h3>Email du contact</h3>
@@ -75,8 +73,7 @@ router.route('/newsletter')
 
 router.route('/contact')
 .post(function(req,res) {
-  console.log(`From Website on contact : `, req.body)
-
+  // console.log(`From Website on contact : `, req.body)
   const output = `
     <p>Vous avez une nouvelle demande de contact</p>
     <h3>Détails du contact</h3>
@@ -127,33 +124,61 @@ router.route('/contact')
   });
 });
 
-router.route(`/event`)
-.post(function(req, res) {
-  res.json({
-    message : `Ajoute un/des participant(s) à un event Martinus`,
-    eventId: req.body.id,
-    name : req.body.name,
-    firstname : req.body.firstname,
-    count : req.body.count,
-    methode: req.method
-  })
-})
-
 router.route('/shop')
 .post(function(req, res) {
-  res.json({
-    message : `Demande un article du shop Martinus`,
-    itemId: req.body.id,
-    name : req.body.name,
-    firstname : req.body.firstname,
-    count : req.body.count,
-    email: req.body.email,
-    methode: req.method
-  })
-})
+  // console.log(`Commande de nouveaux articles : `, req.body)
+  const output = `
+    <h3>Vous avez une nouvelle commande de t-shirt de :</h3>
+    <ul>
+      <li>Nom : ${req.body.cart.fullName}</li>
+      <li>Prénom : ${req.body.cart.firstName}</li>
+      <li>Ville : ${req.body.cart.city}</li>
+      <li>Email : ${req.body.cart.email}</li>
+      <li>Téléphone : ${req.body.cart.phoneNumber}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>J'adore ces t-shirts, je souhaite commander ${req.body.cart.itemNumber} t-shirt(s) ${req.body.cart.gender} s'il vous plait.
+    Merci de me contacter pour m'indiquer comment les récupérer.</p>
+  `;
+  const mailOptions = {
+    from: "Contact depuis le site :" + req.body.cart.email,
+    to: `${dev}, ${contact}`,
+    subject: `Commande sur la boutique solidaire`,
+    text: `Une nouvelle commande...`,
+    html: output,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({
+        status: 'fail'
+      })
+    }  else {
+      // console.log(`Message sent: %s ${info.messageId}`);
+      // Preview only available when sending through an Ethereal account
+      // console.log(`Preview URL: %s ${nodemailer.getTestMessageUrl(info)}`);
+      res.json({
+        status: 'success',
+        message: `Merci, votre commande est bien envoyée !`
+      })
+    }
+    transporter.sendMail({
+      from: contact,
+      to: req.body.cart.email,
+      subject: `Commande envoyé avec succès.`,
+      text: ` Merci pour votre commande ! 
+        Nous revenons rapidement vers vous pour y donner suite.`
+    }, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Message sent: ' + info.response);
+      }
+    })
+  });
+});
 
-app.use(router)
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
